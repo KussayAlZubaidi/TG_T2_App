45430f55cba7391edd3d42e594e8bc6a339dd887
Kussaynm <48143091+Kussaynm@users.noreply.github.com>
2022-09-14 12:39:00 +1000

    Merged with Login/Register project.

--- app/src/main/java/com/example/tg_patient_profile/MainActivity.java
+++ app/src/main/java/com/example/tg_patient_profile/MainActivity.java
@@ -1,93 +1,37 @@
 package com.example.tg_patient_profile;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.SearchView;
-
-import com.firebase.ui.database.FirebaseRecyclerOptions;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.firebase.database.FirebaseDatabase;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    RecyclerView recyclerView;
-    MainAdapter mainAdapter;
 
-    FloatingActionButton floatingActionButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
 
-        recyclerView= findViewById(R.id.rv);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        FirebaseRecyclerOptions<MainModel> options =
-                new FirebaseRecyclerOptions.Builder<MainModel>()
-                        .setQuery(FirebaseDatabase.getInstance().getReference().child("patients"), MainModel.class)
-                        .build();
-        mainAdapter=new MainAdapter(options);
-        recyclerView.setAdapter(mainAdapter);
+        Button getStartedButton = findViewById(R.id.getStartedButton);
+        Button patientProfileButton = findViewById(R.id.patientProfileButton);
 
-        floatingActionButton=(FloatingActionButton)findViewById(R.id.floatingActionButton);
-        floatingActionButton.setOnClickListener(new View.OnClickListener(){
+        getStartedButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-                startActivity(new Intent(getApplicationContext(),AddActivity.class));
+            public void onClick(View view) {
+                Intent getStartedIntent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(getStartedIntent);
             }
         });
-    }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        mainAdapter.startListening();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        mainAdapter.stopListening();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(@NonNull Menu menu) {
-        getMenuInflater().inflate(R.menu.search,menu);
-        MenuItem item =menu.findItem(R.id.search);
-        SearchView searchView= (SearchView) item.getActionView();
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+        patientProfileButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onQueryTextSubmit(String query) {
-                txtSearch(query);
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String query) {
-                txtSearch(query);
-                return false;
+            public void onClick(View view) {
+                Intent patientProfileIntent = new Intent(MainActivity.this, PatientProfileList.class);
+                startActivity(patientProfileIntent);
             }
         });
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    private void txtSearch(String str){
-
-        FirebaseRecyclerOptions<MainModel> options =
-                new FirebaseRecyclerOptions.Builder<MainModel>()
-                        .setQuery(FirebaseDatabase.getInstance().getReference().child("patients").orderByChild("name").startAt(str).endAt(str+"~"), MainModel.class)
-                        .build();
-
-        mainAdapter=new MainAdapter(options);
-        mainAdapter.startListening();
-        recyclerView.setAdapter(mainAdapter);
     }
 }
